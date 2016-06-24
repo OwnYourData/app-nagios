da63a2f14d5367eea65b11e0dd0b1f5467fa05b8
Christoph Fabianek <christoph.fabianek@gmail.com>
2016-06-25 00:26:37 +0200

    providing package repository

--- setup.R
+++ setup.R
@@ -1,7 +1,7 @@
 my_packages = c('httr', 'tools', 'jsonlite')
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
-    install.packages(p, dependencies = TRUE)
+    install.packages(p, dependencies = TRUE, repos="http://cran.rstudio.com/")
   }
   else {
     cat(paste("Skipping already installed package:", p, "\n"))
